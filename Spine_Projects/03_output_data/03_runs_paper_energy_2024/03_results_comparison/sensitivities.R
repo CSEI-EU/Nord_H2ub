4cc4209dedc9eb1010e7de7486feb052dcdcd66b
Dana Josephine Hentschel <djh.eco@cbs.dk>
2024-11-25 17:03:44 +0100

    sensitivity analysis in R

--- Spine_Projects/03_output_data/03_runs_paper_energy_2024/03_results_comparison/sensitivities.R
+++ Spine_Projects/03_output_data/03_runs_paper_energy_2024/03_results_comparison/sensitivities.R
@@ -5,25 +5,28 @@ graphics.off()
 setwd("C:/Users/djh.eco/OneDrive - CBS - Copenhagen Business School/Documents/GitHub/Nord_H2ub/Spine_Projects/03_output_data/03_runs_paper_energy_2024")
 
 #Packages
-library(stargazer)
-library(readxl)
-library(goeveg)
-library(readr)
-library(ivreg)
-library(car)
-library(estimatr)
-library(ggfortify)
-library(wooldridge)
-library(AER)
-library(forecast)
-library(urca)
-library(strucchange)
-library(readr)
-library(stats)
-library(tidyverse)
-library(xtable)
-library(ggplot2)
-library(scales)
+{
+  library(stargazer)
+  library(readxl)
+  library(goeveg)
+  library(readr)
+  library(ivreg)
+  library(car)
+  library(estimatr)
+  library(ggfortify)
+  library(wooldridge)
+  library(AER)
+  library(forecast)
+  library(urca)
+  library(strucchange)
+  library(readr)
+  library(stats)
+  library(tidyverse)
+  library(xtable)
+  library(ggplot2)
+  library(scales)
+}
+
 
 #Import data
 {
@@ -54,10 +57,10 @@ library(scales)
   lifetime05pup = as.data.frame(read_excel("02_output_prepared/output_sens_10op_lifetime_05pup_run.xlsx", sheet = "LCOE"))
   lifetime10pup = as.data.frame(read_excel("02_output_prepared/output_sens_10op_lifetime_10pup_run.xlsx", sheet = "LCOE"))
   
-  #variance10pdown = as.data.frame(read_excel("02_output_prepared/output_sens_10op_elpricevar_10pdown_run.xlsx", sheet = "LCOE"))
-  #variance05pdown = as.data.frame(read_excel("02_output_prepared/output_sens_10op_elpricevar_05pdown_run.xlsx", sheet = "LCOE"))
-  #variance05pup = as.data.frame(read_excel("02_output_prepared/output_sens_10op_elpricevar_05pup_run.xlsx", sheet = "LCOE"))
-  #variance10pup = as.data.frame(read_excel("02_output_prepared/output_sens_10op_elpricevar_10pup_run.xlsx", sheet = "LCOE"))
+  variance10pdown = as.data.frame(read_excel("02_output_prepared/output_sens_10op_elpricevar_10pdown_run.xlsx", sheet = "LCOE"))
+  variance05pdown = as.data.frame(read_excel("02_output_prepared/output_sens_10op_elpricevar_05pdown_run.xlsx", sheet = "LCOE"))
+  variance05pup = as.data.frame(read_excel("02_output_prepared/output_sens_10op_elpricevar_05pup_run.xlsx", sheet = "LCOE"))
+  variance10pup = as.data.frame(read_excel("02_output_prepared/output_sens_10op_elpricevar_10pup_run.xlsx", sheet = "LCOE"))
   
   invc10pdown = as.data.frame(read_excel("02_output_prepared/output_sens_10op_invc_10pdown_run.xlsx", sheet = "LCOE"))
   invc05pdown = as.data.frame(read_excel("02_output_prepared/output_sens_10op_invc_05pdown_run.xlsx", sheet = "LCOE"))
@@ -82,9 +85,9 @@ lifetime = rbind(lifetime10pdown, lifetime05pdown, base, lifetime05pup, lifetime
 lifetime = lifetime %>% 
   filter(!grepl('_PV', run_name))
 
-#variance = rbind(variance10pdown, base, variance10pup)
-#variance = variance %>% 
-  #filter(!grepl('_PV', run_name))
+variance = rbind(variance10pdown, variance05pdown, base, variance05pup, variance10pup)
+variance = variance %>% 
+  filter(!grepl('_PV', run_name))
 
 invc = rbind(invc10pdown, invc05pdown, base, invc05pup, invc10pup)
 invc = invc %>% 
@@ -97,61 +100,23 @@ dhprice = dhprice %>%
 
 # Combined into one df
 #add variance and demand when run
-data = setNames(data.frame(matrix(ncol = 6, nrow = 5)), c("percent", "elprice", "wacc", "lifetime", "invc", "dhprice"))
+data = setNames(data.frame(matrix(ncol = 7, nrow = 5)), c("percent", "elprice", "wacc", "lifetime", "invc", "dhprice", "elpricevar"))
 data$percent = c("-10%", "-5%", "0", "5%", "10%")
 #data$demand = demand$`LCOE [Euro/t]`
 data$elprice = elprice$`LCOE [Euro/t]`
 data$wacc = wacc$`LCOE [Euro/t]`
 data$lifetime = lifetime$`LCOE [Euro/t]`
-#data$variance = variance$`LCOE [Euro/t]`
+data$variance = variance$`LCOE [Euro/t]`
 data$invc = invc$`LCOE [Euro/t]`
 data$dhprice = dhprice$`LCOE [Euro/t]`
 data$percent <- as.numeric(gsub("%", "", data$percent))
 
 
 # Graph investment
-max.lcoe.inv = max(data$wacc, data$lifetime, data$invc)
-min.lcoe.inv = min(data$wacc, data$lifetime, data$invc)
+max.lcoe = max(data$wacc, data$lifetime, data$invc, data$elprice, data$dhprice, data$variance) #add demand
+min.lcoe = min(data$wacc, data$lifetime, data$invc, data$elprice, data$dhprice, data$variance) #add demand
 
-ggplot(data, aes(x = percent, group = 1)) +
-  geom_line(aes(y = wacc, color = "WACC"), linewidth = 1) +
-  geom_point(aes(y = wacc, color = "WACC"), size = 3) +
-  geom_line(aes(y = lifetime, color = "Lifetime"), linewidth = 1) + 
-  geom_point(aes(y = lifetime, color = "Lifetime"), size = 3) +
-  geom_line(aes(y = invc, color = "Investment costs"), linewidth = 1) + 
-  geom_point(aes(y = invc, color = "Investment costs"), size = 3) +
-  scale_y_continuous(
-    name = "LCOE [Euro/t]",
-    limits = c(0.99*min.lcoe.inv, 1.005*max.lcoe.inv),
-    breaks = seq(min.lcoe.inv, max.lcoe.inv, (max.lcoe.inv - min.lcoe.inv)/4),
-    labels = label_number(accuracy = 0.01),
-  ) +
-  scale_x_continuous(
-    name = "Percentage change in value",  # Label for x-axis
-    minor_breaks = seq(min(data$percent), max(data$percent), by = 1),
-    breaks = seq(min(data$percent), max(data$percent), by = 5)
-  ) +
-  theme_bw() +
-  labs(color = "") +
-  scale_color_manual(breaks=c("WACC", "Lifetime", "Investment costs"),
-                     values=c("WACC" = "#4967AA", "Lifetime" = "#52A596", "Investment costs" = "#E66A57")) +
-  theme(
-    axis.title.x = element_text(size = 12, face = "bold"),
-    axis.text.x = element_text(face = "bold"),
-    axis.title.y = element_text(color = "black", size = 12, face = "bold"),
-    axis.text.y = element_text(color = "black", face = "bold"),
-    panel.grid.major = element_line(color = "gray98", linewidth = 0.5),
-    panel.grid.minor = element_line(color = "gray98", linewidth = 0.5),
-    legend.position = c(0.5, 0.1),
-    legend.text = element_text(size = 10),
-    legend.direction = "horizontal"
-  )
-
-# Graph investment
-max.lcoe.inv = max(data$wacc, data$lifetime, data$invc) #add demand
-min.lcoe.inv = min(data$wacc, data$lifetime, data$invc) #add demand
-
-ggplot(data, aes(x = percent, group = 1)) +
+plot.inv = ggplot(data, aes(x = percent, group = 1)) +
   geom_line(aes(y = wacc, color = "WACC"), linewidth = 1) +
   geom_point(aes(y = wacc, color = "WACC"), size = 3) +
   geom_line(aes(y = lifetime, color = "Lifetime"), linewidth = 1) + 
@@ -162,8 +127,8 @@ ggplot(data, aes(x = percent, group = 1)) +
   #geom_point(aes(y = invc, color = "Demand"), size = 3) +
   scale_y_continuous(
     name = "LCOE [Euro/t]",
-    limits = c(0.99*min.lcoe.inv, 1.005*max.lcoe.inv),
-    breaks = seq(min.lcoe.inv, max.lcoe.inv, (max.lcoe.inv - min.lcoe.inv)/4),
+    limits = c(0.99*min.lcoe, 1.005*max.lcoe),
+    breaks = seq(min.lcoe, max.lcoe, (max.lcoe - min.lcoe)/4),
     labels = label_number(accuracy = 0.01),
   ) +
   scale_x_continuous(
@@ -173,7 +138,7 @@ ggplot(data, aes(x = percent, group = 1)) +
   ) +
   theme_bw() +
   labs(color = "") +
-  scale_color_manual(breaks=c("WACC", "Lifetime", "Investment costs", "Demand"), #add demand
+  scale_color_manual(breaks=c("WACC", "Lifetime", "Investment costs", "Demand"),
                      values=c("WACC" = "#4967AA", "Lifetime" = "#52A596", "Investment costs" = "#E66A57", "Demand" = "yellow")) +
   theme(
     axis.title.x = element_text(size = 12, face = "bold"),
@@ -186,24 +151,22 @@ ggplot(data, aes(x = percent, group = 1)) +
     legend.text = element_text(size = 10),
     legend.direction = "horizontal"
   )
+#ggsave("04_images/sensitivities_inv.png", plot = plot.inv, width = 7.5, height = 5, dpi = 300)
 
 
 # Graph prices
-max.lcoe.elec = max(data$elprice, data$variance) #add variance
-min.lcoe.elec = min(data$elprice, data$variance) #add variance
-
-ggplot(data, aes(x = percent, group = 1)) +
+plot.price = ggplot(data, aes(x = percent, group = 1)) +
   geom_line(aes(y = elprice, color = "Electricity price"), linewidth = 1) +
   geom_point(aes(y = elprice, color = "Electricity price"), size = 3) +
   geom_line(aes(y = dhprice, color = "District heating price"), linewidth = 1) + 
   geom_point(aes(y = dhprice, color = "District heating price"), size = 3) +
-  #geom_line(aes(y = variance, color = "Electricity price variance"), linewidth = 1) + 
-  #geom_point(aes(y = variance, color = "Electricity price variance"), size = 3) +
+  geom_line(aes(y = variance, color = "Electricity price variance"), linewidth = 1) + 
+  geom_point(aes(y = variance, color = "Electricity price variance"), size = 3) +
   scale_y_continuous(
     name = "LCOE [Euro/t]",
-    limits = c(0.99*min.lcoe.elec, 1.005*max.lcoe.elec),
-    breaks = seq(min.lcoe.elec, max.lcoe.elec, (max.lcoe.elec - min.lcoe.elec)/4),
-    labels = label_number(accuracy = 0.01),
+    limits = c(0.99*min.lcoe, 1.005*max.lcoe),
+    breaks = seq(min.lcoe, max.lcoe, (max.lcoe - min.lcoe)/4),
+    labels = label_number(accuracy = 0.01)
   ) +
   scale_x_continuous(
     name = "Percentage change in value",  # Label for x-axis
@@ -226,5 +189,4 @@ ggplot(data, aes(x = percent, group = 1)) +
     legend.direction = "horizontal"
   )
 
-
-#ggsave("04_images/sensitivities.png", plot = plot, width = 7.5, height = 4, dpi = 300)
+#ggsave("04_images/sensitivities_prices.png", plot = plot.price, width = 7.5, height = 5, dpi = 300)
