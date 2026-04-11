1a962d3031a67b2b6a951adb4d5a86abb45b1873
minecraft-cebula <kacper.rokosz@gmail.com>
2026-04-11 14:07:38 +0200

    fixed issue #138 - updated district heating mapping

--- spine_projects/01_input_data/00_functions/nord_h2ub_data_preparation_functions.py
+++ spine_projects/01_input_data/00_functions/nord_h2ub_data_preparation_functions.py
@@ -1213,8 +1213,8 @@ def translate_product(product):
 def get_district_heating_sheets(run_name):
 
     dh__definition = pd.DataFrame({
-        "Object_name": ["pth_dummy_unit", "pth_dummy_node", "excess_heat", "excess_heat_exchanger", "heat_recovery", "dh_heat_exchanger"],
-        "Category":    ["unit", "node", "node", "unit", "investment_group", "unit"]
+        "Object_name": ["pth_dummy_unit", "pth_dummy_node", "excess_heat_exchanger", "heat_recovery", "dh_heat_exchanger"],
+        "Category":    ["unit", "node", "unit", "investment_group", "unit"]
     })
 
     dh__definition_parameters = pd.DataFrame({
@@ -1238,16 +1238,16 @@ def get_district_heating_sheets(run_name):
     })
 
     df__nodes = pd.DataFrame({
-        "Object_name":          ["pth_dummy_node", "excess_heat"],
-        "Category":             ["node", "node"],
-        "balance_type":         ["balance_type_node", "balance_type_node"],
-        "Alternative":          [run_name, run_name],
-        "nodal_balance_sense":  [None, ">="],
-        "has_state":            [None, None],
-        "node_state_cap":       [None, None],
-        "frac_state_loss":      [None, None],
-        "demand":               [None, None],
-        "node_slack_penalty":   [100000000, 100000000]
+        "Object_name":          ["pth_dummy_node"],
+        "Category":             ["node"],
+        "balance_type":         ["balance_type_node"],
+        "Alternative":          [run_name],
+        "nodal_balance_sense":  [None],
+        "has_state":            [None],
+        "node_state_cap":       [None],
+        "frac_state_loss":      [None],
+        "demand":               [None],
+        "node_slack_penalty":   [100000000]
     })
 
     dh__object__to_from_node_definition = pd.DataFrame({
@@ -1270,24 +1270,24 @@ def get_district_heating_sheets(run_name):
     })
 
     dh__object__node_node_def = pd.DataFrame({
-        "Relationship":    ["unit__node__node", "unit__node__node", "unit__node__node", "unit__node__node", "unit__node__node", "unit__node__node", "unit__node__node", "unit__to_node__investment_group", "unit__from_node__investment_group"],
-        "Object_class_1":  ["unit", "unit", "unit", "unit", "unit", "unit", "unit", "unit", "unit"],
-        "Object_name_1":   ["steam_plant", "steam_plant", "electrolyzer", "ch3oh_reactor", "pth_dummy_unit", "excess_heat_exchanger", "dh_heat_exchanger", "excess_heat_exchanger", "steam_plant"],
-        "Object_class_2":  ["node", "node", "node", "node", "node", "node", "node", "node", "node"],
-        "Object_name_2":   ["pth_dummy_node", "pth_dummy_node", "h2", "raw_ch3oh", "power", "excess_heat", "excess_heat", "pth_dummy_node", "pth_dummy_node"],
-        "Object_class_3":  ["node", "node", "node", "node", "node", "node", "node", "investment_group", "investment_group"],
-        "Object_name_3":   ["water", "steam", "excess_heat", "excess_heat", "pth_dummy_node", "pth_dummy_node", "heat", "heat_recovery", "heat_recovery"]
+        "Relationship":    ["unit__to_node__investment_group", "unit__from_node__investment_group"],
+        "Object_class_1":  ["unit", "unit"],
+        "Object_name_1":   ["excess_heat_exchanger", "steam_plant"],
+        "Object_class_2":  ["node", "node"],
+        "Object_name_2":   ["pth_dummy_node", "pth_dummy_node"],
+        "Object_class_3":  ["investment_group", "investment_group"],
+        "Object_name_3":   ["heat_recovery", "heat_recovery"]
     })
 
     dh__object__node_node = pd.DataFrame({
-        "Relationship": ["unit__node__node", "unit__node__node", "unit__node__node", "unit__node__node", "unit__node__node", "unit__node__node", "unit__node__node"],
-        "Object_class": ["unit", "unit", "unit", "unit", "unit", "unit", "unit"],
-        "Object_name":  ["steam_plant", "steam_plant", "electrolyzer", "ch3oh_reactor", "pth_dummy_unit", "excess_heat_exchanger", "dh_heat_exchanger"],
-        "Node1":        ["pth_dummy_node", "pth_dummy_node", "h2", "raw_ch3oh", "power", "excess_heat", "excess_heat"],
-        "Node2":        ["water", "steam", "excess_heat", "excess_heat", "pth_dummy_node", "pth_dummy_node", "heat"],
-        "Parameter":    ["fix_ratio_in_in_unit_flow", "fix_ratio_in_out_unit_flow", "fix_ratio_out_out_unit_flow", "fix_ratio_out_out_unit_flow", "fix_ratio_in_out_unit_flow", "fix_ratio_in_out_unit_flow", "fix_ratio_in_out_unit_flow"],
-        "Value":        [0.000724378, 0.99, 1.76, 4.32, 1, 10, 1],
-        "Alternative":  [run_name, run_name, run_name, run_name, run_name, run_name, run_name]
+        "Relationship": ["unit__node__node", "unit__node__node", "unit__node__node", "unit__node__node", "unit__node__node"],
+        "Object_class": ["unit", "unit", "unit", "unit", "unit"],
+        "Object_name":  ["steam_plant", "steam_plant", "pth_dummy_unit", "excess_heat_exchanger", "dh_heat_exchanger"],
+        "Node1":        ["pth_dummy_node", "pth_dummy_node", "power", "excess_heat", "excess_heat"],
+        "Node2":        ["water", "steam", "pth_dummy_node", "pth_dummy_node", "heat"],
+        "Parameter":    ["fix_ratio_in_in_unit_flow", "fix_ratio_in_out_unit_flow", "fix_ratio_in_out_unit_flow", "fix_ratio_in_out_unit_flow", "fix_ratio_in_out_unit_flow"],
+        "Value":        [0.000724378, 0.99, 0.991, 10, 1],
+        "Alternative":  [run_name, run_name, run_name, run_name, run_name]
     })
 
     dh_sheets_mapping = {
